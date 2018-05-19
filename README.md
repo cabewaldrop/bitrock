b0f0df41802accf1700d341db6d4fd59a61283bf
Cabe <cabe.waldrop@bbva.com>
2018-05-19 09:22:08 -0600

    Setting up frontend and tying it to backend build

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ This will start the app on port 8080 and you can visit it at localhost:8080.
 To run the frontend you will also need yarn.  This can be installed with homebrew as well
 
 `brew install yarn`
+
+Next install webpack in the frontend directory
+
+`npm i -D webpack-cli`
+
+Once you have yarn installed you will need to add webpack for bundling your js assets and running a dev server
+
+`yarn add --dev webpack webpack-dev-server`
+
--- backend/src/main/java/com/bitrock/backend/core/Application.java
+++ backend/src/main/java/com/bitrock/backend/core/Application.java
@@ -6,17 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan({"com.bitrock.backend.core", "com.bitrock.backend.dev"})
 public class Application extends SpringBootServletInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
 
     public static void main(String[] args) { SpringApplication.run(Application.class, args);}
 }
